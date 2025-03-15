9573802b7ad96f7c4f29cb9751e6ba097aaae6c4
Anna Fomina <ann.fomi77@gmail.com>
2025-03-15 21:08:09 +0200

    Initial commit

---
+++ TodoList - Copy.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function TodoList() {
+    let todos = [
+        'Go to the gym',
+        'Eat more fruits and vege',
+        'Pick up the kids from school'
+    ]
+
+    return (
+        <ul className='main'>{todos.map((todo, todoIndex) => {
+            return (
+                <li className='todoItem' key={todoIndex}>{todo}<i class="fa-solid fa-pen-to-square"></i></li>
+            )
+        })}</ul>
+    )
+}
